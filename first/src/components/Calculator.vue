9e99740d8a322fa01e52f4f1d0faf9186beb7e7e
Alexey <mctasb@yandex.ru>
2021-07-01 13:58:02 +0300

    Lesson_2

--- first/src/components/Calculator.vue
+++ first/src/components/Calculator.vue
@@ -1,20 +1,62 @@
 <template>
   <div>
-    <input type="number" v-model.number="number1" placeholder="number1" />
-
-    <div class="keyboard">
-      <button v-on:click="sum = number1 + number2">+</button>
-      <button v-on:click="sum = number1 - number2">-</button>
-      <button @click="mult">*</button>
-      <button @click="foo(number1, number2)">/</button>
-      <button @click="level(number1, number2)">в степени</button>
-      <button @click="level2(number1, number2)">Ц/Ч деление</button>
+    <div v-for="(item, idx) in myCollection" :key="`${idx}_parent`">
+      {{ item }} - {{ idx }}
     </div>
 
-    <input type="number" v-model.number="number2" placeholder="number2" />
-    <br />
-    равно
-    <h3>{{ sum }}</h3>
+    <div class="logs" v-for="(log, id) in logs" :key="id">
+      {{ log }}
+    </div>
+
+    <input type="number" v-model.lazy="operand1" placeholder="operand1" />
+    <input type="number" v-model.lazy="operand2" placeholder="operand2" />
+    <div>= {{ result }}</div>
+    <div>= fib {{ fibResult }}</div>
+
+    <div class="error" :class="{ red: error }" v-if="error">
+      Ошибка!! {{ error }}
+    </div>
+
+    <div class="error" :class="{ red: error }" v-show="!!error">
+      Ошибка!! {{ error }}
+    </div>
+
+    <div class="strange-message">
+      <template v-if="result < 0"> Получилось отрицательное число </template>
+      <template v-else-if="result < 100"
+        >Получилось число в первой сотне</template
+      >
+      <template v-else>Все остальные числа</template>
+    </div>
+
+    <div class="keyboard">
+      <button
+        class=""
+        v-for="operand in operands"
+        @click="calculate(operand)"
+        :key="operand"
+        v-bind:title="operand"
+      >
+        {{ operand }}
+      </button>
+    </div>
+    <input type="checkbox" v-on:click="visible = !visible" />
+    <label for="checkbox">Включить экранную клавиатуру</label><br />
+    <div v-show="visible" class="numbers">
+      <button
+        v-for="number in myNumbers"
+        @click="getOper1()"
+        :key="number"
+        v-bind:title="number"
+      >
+        {{ number }}
+      </button>
+      <button @click="this.operand1.slice(0, -1)">Delete</button>
+    </div>
+    <div>
+      <input type="checkbox" :onchange.prop="getOper1" />Операнд 1
+      <input type="checkbox" :onchange.prop="getOper2" />Операнд 2
+    </div>
   </div>
 </template>
 
@@ -23,33 +65,86 @@ export default {
   name: "Calculator",
   data() {
     return {
-      number1: 0,
-      number2: 0,
-      sum: 0,
+      myNumbers: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"],
+      operands: ["+", "-", "/", "*"],
+
+      text1: "",
+      operand1: 0,
+      operand2: 0,
+      fibResult: 0,
+      result: 0,
+      error: "",
+      logs: {},
+      visible: false,
     };
   },
-  methods: {
-    mult() {
-      this.sum = this.number1 * this.number2;
-    },
-    foo(number1, number2) {
-      this.sum = number1 / number2;
-    },
-    level(num1, num2) {
-      this.sum = num1 ** num2;
-    },
-    level2(number1, number2) {
-      if (number2 != 0) {
-        this.sum = parseInt(this.number1 / this.number2);
 
-        console.log(this.sum);
-      } else {
-        this.sum = "Error";
+  methods: {
+    calculate(operation = "+") {
+      this.error = "";
+      switch (operation) {
+        case "+":
+          this.add();
+          break;
+        case "-":
+          this.substract();
+          break;
+        case "*":
+          this.multiply();
+          break;
+        case "/":
+          this.divide();
+          break;
       }
+      const key = Date.now();
+      const value = `${this.operand1}${operation}${this.operand2} = ${this.result}`;
+      this.$set(this.logs, key, value);
+    },
+    getOper1() {
+      this.operand1 = this.operand1 + this.number;
+    },
+    getOper2() {
+      this.operand2 = this.operand2 + this.number;
+    },
+    add() {
+      this.result = this.operand1 + this.operand2;
+      this.fibResult = this.fib1 + this.fib2;
+    },
+    substract() {
+      console.log("substract");
+      this.result = this.operand1 - this.operand2;
+    },
+    multiply() {
+      this.result = this.operand1 * this.operand2;
+    },
+    divide() {
+      const { operand1, operand2 } = this;
+
+      if (operand2 === 0) {
+        return (this.error = "Делить на 0 нельзя");
+      }
+
+      this.result = operand1 / operand2;
+    },
+    sendData(data) {
+      console.log("Send Data name", data);
+    },
+    fib(n) {
+      return n <= 1 ? n : this.fib(n - 1) + this.fib(n - 2);
+    },
+  },
+  computed: {
+    fib1() {
+      return this.fib(this.operand1);
+    },
+    fib2() {
+      return this.fib(this.operand2);
     },
   },
 };
 </script>
-
-<style>
+<style scoped>
+.red {
+  color: red;
+}
 </style>
