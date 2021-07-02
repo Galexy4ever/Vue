0c7c91db3f6aed730aef6bea7d48417c26ab532c
Alexey <mctasb@yandex.ru>
2021-07-02 15:55:26 +0300

    Lessin_2_final

--- first/src/components/Calculator.vue
+++ first/src/components/Calculator.vue
@@ -1,15 +1,21 @@
 <template>
   <div>
-    <div v-for="(item, idx) in myCollection" :key="`${idx}_parent`">
-      {{ item }} - {{ idx }}
-    </div>
-
     <div class="logs" v-for="(log, id) in logs" :key="id">
       {{ log }}
     </div>
 
-    <input type="number" v-model.lazy="operand1" placeholder="operand1" />
-    <input type="number" v-model.lazy="operand2" placeholder="operand2" />
+    <input
+      type="number"
+      v-model.lazy="operand1"
+      placeholder="operand1"
+      v-bind:disabled="check !== 1"
+    />
+    <input
+      type="number"
+      v-model.lazy="operand2"
+      placeholder="operand2"
+      v-bind:disabled="check !== 2"
+    />
     <div>= {{ result }}</div>
     <div>= fib {{ fibResult }}</div>
 
@@ -46,18 +52,18 @@
       <button
         class=""
         v-for="number in myNumbers"
-        @click="getOper1(number)"
+        @click="getOper(number)"
         :key="number"
         v-bind:title="number"
       >
         {{ number }}
       </button>
 
-      <button @click="operand1.slice(' ')">Delete</button>
+      <button @click="del()">Delete</button>
     </div>
     <div>
-      <input id="555" type="checkbox" :onchange.prop="getOper1" />Операнд 1
-      <input id="444" type="checkbox" :onchange.prop="getOper2" />Операнд 2
+      <input name="inputBut" type="radio" :onchange.prop="getOper1" />Операнд 1
+      <input name="inputBut" type="radio" :onchange.prop="getOper2" />Операнд 2
     </div>
   </div>
 </template>
@@ -67,12 +73,13 @@ export default {
   name: "Calculator",
   data() {
     return {
-      myNumbers: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"],
+      myNumbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
       operands: ["+", "-", "/", "*"],
-
+      check: "",
+      checked: "",
       text1: "",
-      operand1: 0,
-      operand2: 0,
+      operand1: "",
+      operand2: "",
       fibResult: 0,
       result: 0,
       error: "",
@@ -102,33 +109,45 @@ export default {
       const value = `${this.operand1}${operation}${this.operand2} = ${this.result}`;
       this.$set(this.logs, key, value);
     },
-    getOper1(num) {
-      console.log(this.num);
-      console.log(this.operand1);
-      this.operand1 = this.operand1 + num;
+    del() {
+      if (this.check == 1) {
+        this.operand1 = this.operand1.slice(0, -1);
+      } else {
+        this.operand2 = this.operand2.slice(0, -1);
+      }
     },
-    getOper2(num) {
-      this.operand2 = this.operand2 + num;
+    getOper(num) {
+      if (this.check == 1) {
+        this.operand1 = this.operand1 + String(num);
+      } else {
+        this.operand2 = this.operand2 + String(num);
+      }
+    },
+    getOper1() {
+      this.check = 1;
+    },
+    getOper2() {
+      this.check = 2;
     },
     add() {
-      this.result = this.operand1 + this.operand2;
+      this.result = +this.operand1 + +this.operand2;
       this.fibResult = this.fib1 + this.fib2;
     },
     substract() {
       console.log("substract");
-      this.result = this.operand1 - this.operand2;
+      this.result = +this.operand1 - +this.operand2;
     },
     multiply() {
-      this.result = this.operand1 * this.operand2;
+      this.result = +this.operand1 * +this.operand2;
     },
     divide() {
       const { operand1, operand2 } = this;
 
-      if (operand2 === 0) {
+      if (+operand2 === 0) {
         return (this.error = "Делить на 0 нельзя");
       }
 
-      this.result = operand1 / operand2;
+      this.result = +operand1 / +operand2;
     },
     sendData(data) {
       console.log("Send Data name", data);
